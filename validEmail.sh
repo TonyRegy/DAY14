1f2ad62fe64d7c61348c9988f4bfe7e1a7708c8c
TonyRegy <tonyregy@gmail.com>
2020-05-20 21:04:53 +0530

    email

---
+++ validEmail.sh
@@ -0,0 +1,11 @@
+O#!/bin/bash -x
+shopt -s extglob
+echo "enter emailAddress"
+read emailAddress
+pat="^[a-zA-Z0-9.-*#!]+\.[a-zA-Z]*@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}\.[A-Za-z]*$"
+if [[ $emailAddress =~ $pat ]]
+then
+	echo "valid email"
+else
+	echo "invalid email"
+fi
