8741869c4c1db366eab87e942b05ebfd0f365941
TonyRegy <tonyregy@gmail.com>
2020-05-20 21:06:46 +0530

    mobile number

---
+++ mobileNumber.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+shopt -s  extglob
+echo "enter mobileNumber"
+read mobileNumber
+pat="^[0-9]{2}[]{1}[0-9]{10}$"
+if [[ $mobileNumber =~ $pat ]]
+then
+	echo "mobile number is valid"
+else
+	echo "mobile number is invalid"
+fi
