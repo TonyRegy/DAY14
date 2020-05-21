133dfd743ebfbf4b5feedf0d8d6705ba8d5392b6
TonyRegy <tonyregy@gmail.com>
2020-05-21 10:48:19 +0530

    new validemail

--- validEmail.sh
+++ validEmail.sh
@@ -1,8 +1,8 @@
-O#!/bin/bash -x
+#!/bin/bash -x
 shopt -s extglob
 echo "enter emailAddress"
 read emailAddress
-pat="^[a-zA-Z0-9.-*#!]+\.[a-zA-Z]*@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}\.[A-Za-z]*$"
+pat="^[a-zA-Z0-9]+[a-zA-Z0-9_-]+\.[a-zA-Z]*@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}\.[A-Za-z]*$"
 if [[ $emailAddress =~ $pat ]]
 then
 	echo "valid email"
